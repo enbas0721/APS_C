66e0a5fd9f85ca47443c3965ad01f6a61c44705a
enbas0721 <chicken.72177@gmail.com>
2022-11-24 20:40:14 +0900

    test

--- Estimator/alsa-record-example.c
+++ Estimator/alsa-record-example.c
@@ -9,61 +9,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
+#include "WavManager/audioio.h"
 #include <time.h>
 
 #define SMPL 44100
 #define BIT 16
 
-void make_wave_header(FILE *fp, size_t size){
-	size_t filesize;
-	unsigned char head[44];
-
-	/* RIFFヘッダ (8バイト) */
-	memcpy(head, "RIFF", 4);
-	filesize = sizeof(head) + size;
-	head[4] = (filesize - 8) >> 0  & 0xff;
-	head[5] = (filesize - 8) >> 8  & 0xff;
-	head[6] = (filesize - 8) >> 16 & 0xff;
-	head[7] = (filesize - 8) >> 24 & 0xff;
-
-	/* WAVEヘッダ (4バイト) */
-	memcpy(head + 8, "WAVE", 4);
-
-	/* fmtチャンク (24バイト) */
-	memcpy(head + 12, "fmt ", 4);
-	head[16] = 16;
-	head[17] = 0;
-	head[18] = 0;
-	head[19] = 0;
-	head[20] = 1;
-	head[21] = 0;
-	head[22] = 1;
-	head[23] = 0;
-	head[24] = SMPL >> 0  & 0xff;
-	head[25] = SMPL >> 8  & 0xff;
-	head[26] = SMPL >> 16 & 0xff;
-	head[27] = SMPL >> 24 & 0xff;
-	head[28] = (SMPL * (BIT / 8)) >> 0  & 0xff;
-	head[29] = (SMPL * (BIT / 8)) >> 8  & 0xff;
-	head[30] = (SMPL * (BIT / 8)) >> 16 & 0xff;
-	head[31] = (SMPL * (BIT / 8)) >> 24 & 0xff;
-	head[32] = (BIT / 8) >> 0 & 0xff;
-	head[33] = (BIT / 8) >> 8 & 0xff;
-	head[34] = BIT >> 0 & 0xff;
-	head[35] = BIT >> 8 & 0xff;
-
-	/* dataチャンク (8 + size バイト) */
-	memcpy(head + 36, "data", 4);
-	head[40] = size >> 0  & 0xff;
-	head[41] = size >> 8  & 0xff;
-	head[42] = size >> 16 & 0xff;
-	head[43] = size >> 24 & 0xff;
-
-	fwrite(head, sizeof(head), 1, fp);
-}
-
 int main (int argc, char *argv[])
 {
+	// バッファ系の変数
 	int i;
 	int err;
 	char *buffer;
@@ -73,13 +27,26 @@ int main (int argc, char *argv[])
 	snd_pcm_hw_params_t *hw_params;
 	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
 
-	FILE *outputfile;
-	unsigned char header[44];
-	//10秒間分のファイルサイズを確保
-	size_t size = 10 * (SMPL * (BIT/8));
-	outputfile = fopen(argv[2], "w");
-	//wavファイル用のheaderを書き出す
-	make_wave_header(outputfile, size);
+	double recording_time = 4.0f;
+
+	// Wavファイル作成用
+	WAV_PRM prm;
+	double *recoded_data;
+	char filename[64] = "output.wav";
+
+	if (argc != 3) {
+		printf("Not enough argument(s). This needs 2 arguments.\n");
+		exit(1);
+	}
+
+	// for (i = 0; n < sizeof(argv[1]); n++) {
+	// 	filename[n] = argv[1][n];
+	// }
+
+	// パラメータコピー
+	prm.fs = SMPL;
+	prm.bits = BIT;
+	prm.L =  pcm.fs * recording_time;
 
 	if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
 		fprintf (stderr, "cannot open audio device %s (%s)\n",
@@ -164,6 +131,7 @@ int main (int argc, char *argv[])
 		exit(1);
 	}
 
+	data = calloc(pcm.L, sizeof(double));
 	// channel数が1なので、8*1
 	buffer = (char*)malloc((buffer_frames * snd_pcm_format_width(format)) / (8 * 1));
 
@@ -171,25 +139,23 @@ int main (int argc, char *argv[])
 
 	time_t start_time = time(NULL);
 	time_t elapsed_time = time(NULL) - start_time;
-	while (elapsed_time < 5.0f) {
+	while (elapsed_time < recording_time) {
 		if ((err = snd_pcm_readi (capture_handle, buffer, buffer_frames)) != buffer_frames) {
 			fprintf (stderr, "read from audio interface failed (%s)\n",
 			         err, snd_strerror (err));
 			exit (1);
+		} else {
+			fprintf(stdout, err);
 		}
-		// fprintf(stdout, "size of buffer  %d\n", sizeof(buffer));
-		// for (int i = 0; i < sizeof(buffer); i++) {
-		// fprintf(outputfile, "%s", buffer[i]);
-		// fwrite(buffer, sizeof(buffer), 1, outputfile);
-		// }
-		fputs(buffer, outputfile);
 		elapsed_time = time(NULL) - start_time;
 	}
 
+	audio_write(data, &prm, filename);
+
 	free(buffer);
+	free(data);
 
 	fprintf(stdout, "buffer freed\n");
-	fclose(outputfile);
 	snd_pcm_close (capture_handle);
 	fprintf(stdout, "audio interface closed\n");
 
