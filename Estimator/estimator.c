c5db3143f41d8529b2ccac5a03e7a6bd5720adc6
enbas0721 <chicken.72177@gmail.com>
2023-01-05 12:45:19 +0900

    test

--- Estimator/estimator.c
+++ Estimator/estimator.c
@@ -18,6 +18,7 @@ int main(int argc, char const *argv[])
     ret = pthread_create(&record_thread, NULL, (void*)record_start, (void*)&info);
     ret = pthread_detach(record_thread);
     if (ret != 0){
+	printf("error\n");
         exit(1);
     }
     printf("Input close to close\n");
