2ea5a8460fb8dae625a3b8df740449b8f56759ea
enbas0721 <chicken.72177@gmail.com>
2023-01-05 12:38:06 +0900

    test

--- Estimator/estimator.c
+++ Estimator/estimator.c
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
     int ret = 0;
 
     ret = pthread_create(&record_thread, NULL, (void*)record_start, (void*)&info);
-    ret = pthread_detach(record_thread,NULL);
+    ret = pthread_detach(record_thread);
     if (ret != 0){
         exit(1);
     }
