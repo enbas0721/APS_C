694342b514c95ee58c095887ac83c9e5e6e7c458
enbas0721 <chicken.72177@gmail.com>
2023-01-10 19:18:28 +0900

    test

--- Estimator/recordManager.c
+++ Estimator/recordManager.c
@@ -139,7 +139,7 @@ void* record_start(record_info *info)
 		current_index = current_index + err;
 		info->last_index = current_index - 1;
 		printf("current_index : %d\n",current_index);
-		if (current_index > data_size - 1){
+		if (current_index + SMPL > data_size){
 			printf("reallocation needed\n");
 			data_size = data_size + SMPL * 30;
 			info->record_data = realloc(info->record_data, data_size);
