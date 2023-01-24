bace92d4fa03e91b088a76077d777c535fc47795
enbas0721 <chicken.72177@gmail.com>
2023-01-24 17:19:00 +0900

    test

--- Observer/signalSender.py
+++ Observer/signalSender.py
@@ -17,7 +17,7 @@ class Sender:
     def makeWave(self):
         samples = np.zeros(int((0.2*self.rate-self.duration*self.rate)/2))
         samples_t = self.amp * np.sin(np.arange(int(self.duration * self.rate)) * self.freq * np.pi * 2 / self.rate)
-        np.concatenate([samples,samples_t,samples])
+        samples = np.concatenate([samples,samples_t,samples])
         print(samples[4430])
 
         return samples
