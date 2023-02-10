ff570bfc3a035f78065b0774be5783d48ea0216f
enbas0721 <chicken.72177@gmail.com>
2023-02-10 15:04:59 +0900

    test

--- Estimator/recordManager.c
+++ Estimator/recordManager.c
@@ -44,9 +44,9 @@ void* record_start(record_info *info)
 	int delayer_num;
 	
 	// fe = 3500.0 / rate;
-	fe2 = 2000 / rate;
-	fe1 = 500 / rate;
-	delta = 500.0 / rate;
+	fe1 = 2000 / rate;
+	fe2 = 8000 / rate;
+	delta = 1000.0 / rate;
 	
 	delayer_num = (int)(3.1 / delta + 0.5) - 1; /*遅延器の数*/
 	if (delayer_num % 2 == 1){
