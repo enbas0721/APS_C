221cd633ad9d0045aa519a2f5b8b77eb0fe41a82
enbas0721 <chicken.72177@gmail.com>
2023-02-16 20:26:43 +0900

    test

--- Observer/playManager.c
+++ Observer/playManager.c
@@ -19,7 +19,7 @@ void make_chirp_wave(int16_t* data, int f0, int f1, float size){
 	int n;
 	for (n = 0; n < size; n++)
 	{
-		data[n] = (int)(10 * sin(2*M_PI * (f0 + ((f1-f0)*n)/(2*size))*n));
+		data[n] = (int)(100 * sin(2*M_PI * (f0 + ((f1-f0)*n)/(2*size))*n));
 		printf("data:%lf\n",(2 * sin(2*M_PI * (f0 + ((f1-f0)*n)/(2*size))*n)));
 	}		
 }
