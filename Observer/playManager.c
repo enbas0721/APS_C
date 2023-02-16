93f0d40bcfbbcb7d9dce34a3c10ea3a7ace536e5
enbas0721 <chicken.72177@gmail.com>
2023-02-17 01:00:33 +0900

    test

--- Observer/playManager.c
+++ Observer/playManager.c
@@ -13,7 +13,7 @@
 #define DEF_FS              48000
 #define DEF_BITPERSAMPLE    16
 #define WAVE_FORMAT_PCM     1
-#define SIGNAL_L			0.5
+#define SIGNAL_L			0.2
 #define INITIAL_F			1700
 #define FINAL_F				1800
 #define BUF_SIZ				2048
