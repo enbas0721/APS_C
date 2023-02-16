99ab497aa91149e9c1718d9e69491618f1ff98b2
enbas0721 <chicken.72177@gmail.com>
2023-02-16 23:30:12 +0900

    test

--- Observer/playManager.c
+++ Observer/playManager.c
@@ -22,7 +22,7 @@ void make_chirp_wave(int16_t* data, int vol, int f0, int f1, int size){
 	{
 		t = n/DEF_FS
 		data[n] = (int)((vol) * sin(2*M_PI * t * (f0 + ((f1-f0)/(2*SIGNAL_L))*t)));
-		printf("data:%lf\n",((vol) * sin(2*M_PI * t * (f0 + ((f1-f0)/(2*SIGNAL_L))*t)));
+		printf("data:%lf\n",((vol) * sin(2*M_PI * t * (f0 + ((f1-f0)/(2*SIGNAL_L))*t))));
 	}		
 }
 
