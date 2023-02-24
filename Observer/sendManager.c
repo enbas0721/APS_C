6b1b9fbc072adfa61e9b3d5a15976e3befedb22d
enbas0721 <chicken.72177@gmail.com>
2023-02-24 14:31:39 +0900

    test

--- Observer/sendManager.c
+++ Observer/sendManager.c
@@ -30,8 +30,7 @@ void make_sin_wave(int16_t* data, int vol, int f, int size){
 	int n;
 	double t;
 	for (n = 0; n < size; n++){
-        t = (double)n/DEF_FS;
-        data[n] = (int)(vol*sin(2*M_PI*SIGNAL_L*DEF_FS*f*t*(1/DEF_FS)));
+        data[n] = (int)(vol*sin(2*(M_PI/DEF_FS)*f*n));
 	}
 }
 
