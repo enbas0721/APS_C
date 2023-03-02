307749a40ac46c8bd01201c280e6c02e80ea9314
enbas0721 <chicken.72177@gmail.com>
2023-03-02 21:33:21 +0900

    test

--- Estimator/trackManager.h
+++ Estimator/trackManager.h
@@ -7,6 +7,7 @@
 
 void write_result(char * filename, double * time, double * distances, int size);
 void make_chirp_wave(int16_t* g);
+int get_max_index(int* S, size_t size);
 void* track_start(record_info *info);
 
 #endif
