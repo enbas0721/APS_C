9cdcadf6585cdb5afb87726a1dfccefcfa9df1d0
enbas0721 <chicken.72177@gmail.com>
2023-03-12 22:42:34 +0900

    test

--- Estimator/trackManager.c
+++ Estimator/trackManager.c
@@ -108,7 +108,7 @@ void* track_start(record_info *info)
 
     int max_index;
 
-    while((info->flag) || (checking_index < info->last_index))
+    while((info->flag) || (checking_index > info->last_index))
     {
         if (info->last_index > checking_index){
             current_time = (double)checking_index / (double)SMPL;
@@ -157,12 +157,12 @@ void* track_start(record_info *info)
                     cross_correlation(cross_correlation_result, info->record_data, ideal_signal, checking_index);
                     max_index = get_max_index(cross_correlation_result, CRSS_WNDW_SIZ);
                     propagation_time = (double)max_index/(double)SMPL;
-                    // printf("伝播サンプル：%d\n伝播時間%lf\n",max_index, propagation_time);
+                    printf("伝播サンプル：%d\n伝播時間%lf\n",max_index, propagation_time);
                     temperature = temp_measure(temperature);
                     v = sound_speed(temperature);
                     distance = propagation_time * v;
-                    // printf("受信時刻: %lf {m}\n", current_time);
-                    // printf("推定距離: %lf {m}\n", distance);
+                    printf("受信時刻: %lf {m}\n", current_time);
+                    printf("推定距離: %lf {m}\n", distance);
                     
                     distances[log_index] = distance;
                     received_time[log_index] = current_time;
