1a48d424b22c8eba0033e1c0db594a2c60e8a290
enbas0721 <chicken.72177@gmail.com>
2023-03-13 16:20:35 +0900

    test

--- Estimator/trackManager.c
+++ Estimator/trackManager.c
@@ -155,7 +155,7 @@ void* track_start(record_info *info)
                     // }
                     cross_correlation(cross_correlation_result, info->record_data, ideal_signal, checking_index);
                     max_index = get_max_index(cross_correlation_result, CRSS_WNDW_SIZ);
-                    propagation_time = (double)max_index/(double)SMPL - 0.098;
+                    propagation_time = 0.1 - (double)max_index/(double)SMPL;
                     temperature = temp_measure(temperature);
                     v = sound_speed(temperature);
                     distance = propagation_time * v;
