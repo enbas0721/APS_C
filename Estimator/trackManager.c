1ec7850c54ef067358543c036e5a148db98ae6af
enbas0721 <chicken.72177@gmail.com>
2023-03-14 16:52:14 +0900

    test

--- Estimator/trackManager.c
+++ Estimator/trackManager.c
@@ -137,7 +137,7 @@ void* track_start(record_info *info)
                     printf("--------------------\n");
                     
                     distances[log_index] = distance;
-                    received_time[log_index] = checking_index - (SMPL*2);
+                    received_time[log_index] = (checking_index - (SMPL*2))/SMPL;
                     log_index += 1;
                     checking_index += SMPL;
                     break;
