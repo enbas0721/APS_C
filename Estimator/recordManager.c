7220947ef9579902d392fb9696f8d6e5f160875e
enbas0721 <chicken.72177@gmail.com>
2023-03-22 15:24:34 +0900

    test

--- Estimator/recordManager.c
+++ Estimator/recordManager.c
@@ -183,7 +183,7 @@ void* record_start(record_info *info)
 	fprintf(stdout, "buffer allocated\n");
 
 	// fe = 3500.0 / rate;
-	fe1 = 10 / (double)rate;
+	fe1 = 200 / (double)rate;
 	fe2 = 4000 / (double)rate;
 	delta = 500.0 / (double)rate;
 	
