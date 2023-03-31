fef1bad7115e0452debed56dc730c71a60145ac0
enbas0721 <chicken.72177@gmail.com>
2023-03-31 19:31:25 +0900

    test

--- Observer/signalSender.py
+++ Observer/signalSender.py
@@ -18,7 +18,7 @@ class Sender:
         samples = np.array([])
         for i in range(int(self.rate * self.duration)):
             n = i/self.rate
-            samples.append(self.amp * 10000 * np.sin(2*np.pi * n * n * (1700+((1800-1700)/2*0.1))))
+            np.append(samples, self.amp * 10000 * np.sin(2*np.pi * n * n * (1700+((1800-1700)/2*0.1))))
         # samples = self.amp * np.sin(np.arange(int(self.duration * self.rate)) * self.freq * np.pi * 2 / self.rate)
 
         return samples
