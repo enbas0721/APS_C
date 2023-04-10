7c8e13ee6a2d01af803a0e4c8d3c0f335fec1ca6
enbas0721 <chicken.72177@gmail.com>
2023-04-10 14:00:36 +0900

    test

---
+++ Estimator/readManager.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "WavManager/audioio.h"
+#include "recordManager.h"
+
+void* read_start(record_info *info)
+{
+    // 変数宣言
+    WAV_PRM prm_in;
+    int16_t *data_in,*buffer;
+    int n;
+    int current_index = 0;
+    char filename[64];
+
+    printf("Input file name: ");
+    scanf("%s",filename);
+
+    data_in = audio_read(&prm_in, filename);
+    while(current_index < prm_in.L)
+    {
+        for (n = 0; n < BUF_SIZ; n++)
+        {
+            info->record_data[current_index];
+            current_index += 1;
+        }
+    }
+    
+     
+}
---
+++ Estimator/simulator.c
@@ -0,0 +1,46 @@
+// 音響測位のためのメインプログラム
+// 第一引数にオーディオカード名 (例. "hw:1")
+// 第二引数に書き出したい録音ファイル名 (例. "output.wav")
+
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include "readManager.h"
+#include "trackManager.h"
+
+int main(int argc, char const *argv[])
+{
+    char command;
+    record_info info;
+    info.flag = 1;
+    strcpy(info.filename,argv[1]);
+    info.last_index = 0;
+    pthread_t record_thread;
+    pthread_t track_thread;
+    int ret = 0;
+    
+    ret = pthread_create(&record_thread, NULL, (void*)record_start, (void*)&info);
+    ret = pthread_create(&track_thread, NULL, (void*)track_start, (void*)&info);
+    printf("Input close to close\n");
+    while (1)
+    {
+        scanf("%c", &command);
+        if (command == 'c'){
+            info.flag = 0;
+            break;
+        }
+    }
+    ret = pthread_join(track_thread,NULL);
+    if (ret != 0){
+	    printf("error\n");
+        exit(1);
+    }
+    ret = pthread_join(record_thread,NULL);
+    if (ret != 0){
+	    printf("error\n");
+        exit(1);
+    }
+    free(info.record_data);
+    printf("All done.\n");
+    return 0;
+}
