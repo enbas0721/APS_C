320cf3d2b6d1f86696de64fd7e51668f3c555803
enbas0721 <chicken.72177@gmail.com>
2023-04-11 06:31:08 +0900

    test

--- Estimator/trackManager.c
+++ Estimator/trackManager.c
@@ -107,7 +107,7 @@ void* track_start(record_info *info)
     int start_sample = 0;
     double epsilon = 0.01;
     
-    int threshold = 1000;
+    int threshold = 500;
 
     double temperature = 20.0;
     double v = sound_speed(temperature);
