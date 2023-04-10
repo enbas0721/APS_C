43d6385c55fc8ae37833e70090aa555f100a7152
enbas0721 <chicken.72177@gmail.com>
2023-04-10 14:14:29 +0900

    test

---
+++ Estimator/readManager.h
@@ -0,0 +1,11 @@
+#ifndef READMANAGER_H
+#define REARDMANAGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "WavManager/audioio.h"
+
+void* read_start(record_info *info);
+
+#endif
