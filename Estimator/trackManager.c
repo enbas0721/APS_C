10b85acab75997697202cb5b3ddf374e13ecbdb0
enbas0721 <chicken.72177@gmail.com>
2023-04-11 09:19:20 +0900

    test

--- Estimator/trackManager.c
+++ Estimator/trackManager.c
@@ -75,7 +75,7 @@ void cross_correlation(double* fai, int16_t* data, int16_t* ideal_sig, int check
             fai[i] += ((data[first_index + j + tau] * ideal_sig[j])/(var_x*var_y));
         }
     }
-    for (int n = 0; n < CRSS_WNDW_SIZ; n++)
+    for (int n = 0; n < 1000; n++)
     {
         if (fai[i] > 0){
             printf("%d:%e\n",n,fai[n]);
