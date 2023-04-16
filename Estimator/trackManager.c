f2c495cd3e10176db3c5e016052a3ab8dd9817f0
enbas0721 <chicken.72177@gmail.com>
2023-04-16 22:49:53 +0900

    test

--- Estimator/trackManager.c
+++ Estimator/trackManager.c
@@ -92,6 +92,9 @@ int get_max_index(double* S, size_t size){
             max_index = i;
         }
     }
+    for (i = 0; i < 150; i++){
+        printf("i:%d,value:%e\n",i,S[i]);
+    }
     return max_index;
 }
 
